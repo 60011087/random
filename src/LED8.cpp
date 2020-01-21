6fc32570d8317a663b4d90cfbfb763e58e8f15d5
Frame <60011087@kmitl.ac.th>
2020-01-21 23:59:33 +0700

    first_try

---
+++ src/LED8.cpp
@@ -0,0 +1,16 @@
+// #include <Arduino.h>
+// #define LED_BuildIn 13
+
+// void LED8_setup()
+// {
+//     Serial.begin(9600);
+//     pinMode(LED_BuildIn,OUTPUT);
+// }
+
+// void LED8_loop()
+// {
+//     digitalWrite(LED_BuildIn, HIGH);
+//     delay(100);
+//     digitalWrite(LED_BuildIn, LOW);
+//     delay(100);
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,63 @@
 #include <Arduino.h>
+#define LED_BuildIn 13
 
-void setup() {
-  // put your setup code here, to run once:
+uint32_t time_slot = 1000;    // us 500 = max speed 
+uint32_t start_time = 0;      // us
+uint32_t brightness = 100;    // percentage
+uint32_t start_time_ch = 0;
+uint32_t time_to_charge = 50000 ; //us
+int32_t charge_direction = 1; // 1 = up , -1 = down
+
+uint8_t current_led;
+void LED8_setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BuildIn, OUTPUT);
+
+  for (uint8_t i = 4; i <= 10; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+  current_led = 0;
+
+  start_time = micros();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void LED8_loop()
+{
+  if (micros() - start_time_ch > time_to_charge)
+  {
+    if (brightness == 0 || brightness == 100)
+    {
+      charge_direction *= -1;
+    }
+    brightness += charge_direction;
+    // Serial.println(brightness);
+    start_time_ch = micros();
+    // digitalWrite(LED_BuildIn, !digitalRead(LED_BuildIn));
+  }
+
+  if (micros() - start_time > time_slot * brightness / 100)
+  {
+    digitalWrite(current_led + 4, LOW);
+  }
+
+  if (micros() - start_time > time_slot)
+  {
+    digitalWrite(current_led + 4, LOW);
+    current_led++;
+    current_led %= 7;
+    digitalWrite(current_led + 4, HIGH);
+    start_time = micros();
+  }
+}
+
+void setup()
+{
+  LED8_setup();
+}
+
+void loop()
+{
+  LED8_loop();
 }
