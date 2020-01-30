6f2d4d19725b24cffeb6cd674085a5b923a8f921
Frame <60011087@kmitl.ac.th>
2020-01-30 17:27:27 +0700

    V3 done

--- src/main.cpp
+++ src/main.cpp
@@ -2,64 +2,88 @@
 #define LED_BuildIn 13
 #define MAX_BRIGHT 100 // percentage
 #define MIN_BRIGHT 0   // percentage
-
-uint32_t start_time[8] = {};
-uint32_t max_time = 10000;                             // us
-uint32_t bright[8] = {10, 20, 15, 40, 65, 60, 70, 100}; // percentage
-
-uint32_t start_ = 0;    // us
-uint32_t time_ = 20000; // us
-int8_t di[8] = {1, -1, 1, -1, 1, -1, 1, 1};
-
-void LED8_setup()
-{
-  Serial.begin(9600);
-  pinMode(LED_BuildIn, OUTPUT);
-  for (uint8_t i = 3; i <= 10; i++)
-  {
-    pinMode(i, OUTPUT);
-  }
-}
-
-void LED8_loop()
-{
-  // uint32_t uptime = ;
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    if (micros() - start_time[i] <= max_time * bright[i] / 100)
-    {
-      digitalWrite(i + 3, HIGH);
-    }
-    else
-    {
-      digitalWrite(i + 3, LOW);
-    }
-    if (micros() - start_time[i] >= max_time)
-    {
-      start_time[i] = micros();
-    }
-  }
-
-  if (micros() - start_ > time_)
-  {
-    for (uint8_t i = 0; i < 8; i++)
-    {
-      if (bright[i] == MAX_BRIGHT || bright[i] == MIN_BRIGHT)
-      {
-        di[i] *= -1;
-      }
-      bright[i] += di[i];
-    }
-    start_ = micros();
-  }
-}
-
+#define start_pin 3
 void setup()
 {
-  LED8_setup();
+  for(int i = 0 ; i < 8 ; i++){
+    pinMode(i+start_pin, OUTPUT);
+  }
 }
+
+#define dutyT 10000
+#define limit_duty_count 17
+uint32_t start[8] = {};
+uint32_t dutyTime = dutyT; // us
+uint32_t dutyTime_divied = dutyT/100;
+uint32_t b = 5;
+uint32_t direction = 1;
+uint8_t count = 0;
+
+uint8_t status[8] = {};
 
 void loop()
 {
-  LED8_loop();
+    for(uint8_t i = 0; i < 8; i++){
+      if(micros() <= start[i] + dutyTime){
+        if( micros() <= dutyTime_divied * b + start[i]){
+          if(status[i] == 0){
+            digitalWrite(i+start_pin,LOW);
+            status[i] = 1;
+          }
+          
+        }
+        else{
+          if(status[i] == 1){
+            digitalWrite(i+start_pin,HIGH);
+            status[i] = 0;
+          }
+        }
+      }
+      else{
+        count++;
+        if(count == limit_duty_count){
+          if(b == MAX_BRIGHT || b == MIN_BRIGHT){
+            direction *= -1;
+          }
+          b += direction;
+          count = 0;
+        }
+        start[i] = micros();
+      }
+    }
 }
+
+
+// int steplight[11] = {0,0,0,0,0,0,0,0,0,0,0} ,statuslight[11] = {0,0,0,0,0,0,0,0,0,0,0} ,stepfix = 101;
+// float speedlight = 70;
+// unsigned long time_now[11] = {0,0,0,0,0,0,0,0,0,0,0} ,timedutycircle = 1010000/speedlight ,n = timedutycircle/stepfix;
+// void led(int pinout){
+//   if(micros() >= time_now[pinout] + timedutycircle){
+//         time_now[pinout] += timedutycircle;
+//         if(statuslight[pinout]==0) 
+//           digitalWrite(pinout,LOW);
+//         else 
+//           digitalWrite(pinout,HIGH);
+//         steplight[pinout]++;
+//         if(steplight[pinout]==stepfix+1){
+//           if(statuslight[pinout]==0) 
+//             statuslight[pinout] = 1;
+//           else 
+//             statuslight[pinout] = 0;
+//          steplight[pinout] = 0;
+//         }
+//   }
+//   if(micros() >= time_now[pinout] + n*steplight[pinout]){
+//         if(statuslight[pinout]==0) digitalWrite(pinout,HIGH);
+//         else digitalWrite(pinout,LOW);
+//   }
+// }
+
+// void setup() {
+//   for(int i=2;i<=10;i++)  pinMode(i, OUTPUT);
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   for(int i=2;i<=10;i++)  led(i);
+// }
